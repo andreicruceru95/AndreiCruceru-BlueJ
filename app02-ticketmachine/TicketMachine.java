afbe9fd0197c5544d3bb30435e25a780cf21f987
andreicruceru95 <56835442+andreicruceru95@users.noreply.github.com>
2020-10-09 14:43:30 +0100

    Update TicketMachine.java
    New functions:
    insertCoin method will add (in pence) 10p 20, 100 or 200 coins to the balance and will show the balance after every coin inserted.
    printTicket method will alow the user to buy a ticket to any selected destination as long as he inserted enough money.
    checkBalance method will allow the user to check his balance.
    askForRefund method will give the user a refund and will clear the balance.

--- app02-ticketmachine/TicketMachine.java
+++ app02-ticketmachine/TicketMachine.java
@@ -1,108 +1,87 @@
+import java.util.*;
+import java.io.*;
+import java.lang.management.*;
+import java.text.*;
+import java.util.*;
 /**
  * TicketMachine models a ticket machine that issues
  * flat-fare tickets.
- * The price of a ticket is specified via the constructor.
- * Instances will check to ensure that a user only enters
- * sensible amounts of money, and will only print a ticket
- * if enough money has been input.
- * 
- * @author David J. Barnes and Michael Kölling
- * @version 2016.02.29
- * 
- * Modified by Student Name
+ * @author Andrei Cruceru 
+ * @version 09102020
  */
 public class TicketMachine
 {
-    // The price of a ticket from this machine.
-    private int price;
-    // The amount of money entered by a customer so far.
+    // We create three tickets with three different prices in pences
+    public Ticket aylesburyTicket=new Ticket("Aylesbury", 220);
+    public Ticket amershamTicket = new Ticket("Amersham", 300);
+    public Ticket highWycombeTicket = new Ticket("High Wycombe", 330);
+    // Enum coin instance
+    public Coin coin;
+    // Initial balance
     private int balance;
-    // The total amount of money collected by this machine.
-    private int total;
-
-    /**
-     * Create a machine that issues tickets of the given price.
-     */
-    public TicketMachine(int cost)
+    
+    //constructor method. Any new TicketMachine has a initial balance of 0
+    public TicketMachine()
     {
-        price = cost;
         balance = 0;
-        total = 0;
     }
-
-    /**
-     * @Return The price of a ticket.
-     */
-    public int getPrice()
+    // Insert coin method. After every coin inserted, you will see the balance on screen
+    public void insertCoin(Coin coin)
     {
-        return price;
+        System.out.println("You have inserted: " + coin + " pences");
+        balance = balance + coin.getValue();
+        System.out.println("Your balance is "+ balance + " pences");
+    } 
+    // Select your destination and print the ticket at the same time
+    public void printTicket(String destination)
+    {
+        if (destination.equals("Aylesbury")){
+             if (balance<220){
+                System.out.println("Insert " + (220 - balance) + " more pences!");
+             }
+             else{
+             aylesburyTicket.printTicket();
+             balance = balance - 220;             
+             System.out.println("Your can get a refund of " + balance + "pences!");
+             }
+        }
+        else if(destination.equals("Amersham")) {
+             if (balance < 300) {
+                System.out.println("Insert " + (300 - balance) + "pences!");
+             }
+             else {
+                amershamTicket.printTicket();
+                balance = balance - 300;
+                System.out.println("Your can get a refund of " + balance + "pences!");
+             }
+        }
+        else if(destination.equals("High Wycombe")) {
+             if (balance<330) {
+                System.out.println("Insert more £" + (330 - balance) + " pences!");
+             }
+             else {
+                highWycombeTicket.printTicket();
+                balance = balance - 330;
+                System.out.println("Your can get a refund of " + balance + "pences!");
+             }
+        }
+        else {
+            System.out.println("We only offer ticket to:");
+            System.out.println("#Aylesbury, #Amersham or #High Wycombe!");
+            System.out.println("Please select a destination between:");
+            System.out.println("Aylesbury, Amersham or High Wycombe");
+        }
     }
-
-    /**
-     * Return The amount of money already inserted for the
-     * next ticket.
-     */
-    public int getBalance()
+    // Check your balance
+    public int checkBalance()
     {
         return balance;
     }
-
-    /**
-     * Receive an amount of money from a customer.
-     * Check that the amount is sensible.
-     */
-    public void insertMoney(int amount)
+    // Ask for a refund and clear the balance
+    public void askForRefund()
     {
-        if(amount > 0) 
-        {
-            balance = balance + amount;
-        }
-        else 
-        {
-            System.out.println("Use a positive amount rather than: " +
-                               amount);
-        }
+       System.out.println("You have been refunded: " + balance + " pences!");
+       balance = 0;
     }
-
-    /**
-     * Print a ticket if enough money has been inserted, and
-     * reduce the current balance by the ticket price. Print
-     * an error message if more money is required.
-     */
-    public void printTicket()
-    {
-        if(balance >= price) 
-        {
-            // Simulate the printing of a ticket.
-            System.out.println("##################");
-            System.out.println("# The BlueJ Line");
-            System.out.println("# Ticket");
-            System.out.println("# " + price + " cents.");
-            System.out.println("##################");
-            System.out.println();
-
-            // Update the total collected with the price.
-            total = total + price;
-            // Reduce the balance by the price.
-            balance = balance - price;
-        }
-        else 
-        {
-            System.out.println("You must insert at least: " +
-                               (price - balance) + " more cents.");
-                    
-        }
-    }
-
-    /**
-     * Return the money in the balance.
-     * The balance is cleared.
-     */
-    public int refundBalance()
-    {
-        int amountToRefund;
-        amountToRefund = balance;
-        balance = 0;
-        return amountToRefund;
-    }
+    
 }
