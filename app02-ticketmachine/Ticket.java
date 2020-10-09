9015ab2a7b619332dd5ba6a67fe6f11634807f1b
andreicruceru95 <56835442+andreicruceru95@users.noreply.github.com>
2020-10-09 14:45:11 +0100

    Create Ticket.java
    This class creates a ticket with a time stamp, a price and a destination.
    A printMethod will print this ticked in the terminal

---
+++ app02-ticketmachine/Ticket.java
@@ -0,0 +1,33 @@
+import java.util.Date;
+/**
+ * This class will provide tickets for the class ticketMachine
+ * @author Andrei Cruceru
+ * @version 07102020
+ */
+
+public class Ticket
+{
+      // Every ticket has a destination, price and time stamp of purchase
+      private String destination;
+      public int price;
+      private Date timeStamp;
+      
+      
+      // Constructor method
+      public Ticket(String destination, int price)
+      {
+          this.destination=destination;
+          this.price = price;
+          timeStamp = new Date();
+      }
+      //print ticket method
+      public void printTicket()
+      {
+           System.out.println("##############################");
+           System.out.println("  " + destination + "  ");
+           System.out.println("#            " + price + "pence        #");
+           System.out.println("#" + timeStamp + "#");
+           System.out.println("##############################");      
+      }
+      
+} 
