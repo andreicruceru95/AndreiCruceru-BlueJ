f25cd0a9b34d250894b3514b4287a3246aa55688
andreicruceru95 <56835442+andreicruceru95@users.noreply.github.com>
2020-10-11 13:04:35 +0100

    Comments
    updated the version of the app
    Changed some Documentation/JavaDoc Comments into Implementation/Block Comments
    Adjusted some brackets

--- app02-ticketmachine/Coin.java
+++ app02-ticketmachine/Coin.java
@@ -2,23 +2,30 @@
  *  This is a enum class that will allow us to select some coins
  *  to buy tickets.
  *  @author Andrei Cruceru
- *  @version 07102020
+ *  @version 11102020
  */
 
 public enum Coin
-{ 
+{
+    // The four types of coins we are going to use
     P10 (10),
     P20 (20),
     P100 (100),
     P200 (200);
 
     public final int value;
-
+    
+    /**
+    * constructor method
+    */    
     private Coin(int value)
     {
         this.value = value;
     }
     
+    /**
+     * Return the value of the coin
+     */
     public int getValue()
     {
         return value;
--- app02-ticketmachine/Ticket.java
+++ app02-ticketmachine/Ticket.java
@@ -2,32 +2,37 @@ import java.util.Date;
 /**
  * This class will provide tickets for the class ticketMachine
  * @author Andrei Cruceru
- * @version 07102020
+ * @version 11102020
  */
 
 public class Ticket
 {
-      // Every ticket has a destination, price and time stamp of purchase
-      private String destination;
-      public int price;
-      private Date timeStamp;
-      
-      
-      // Constructor method
-      public Ticket(String destination, int price)
-      {
-          this.destination=destination;
+    // Every ticket has a destination, price and time stamp of purchase
+    private String destination;
+    public int price;
+    private Date timeStamp;
+        
+    /**
+     * Constructor method for this class.
+     */
+    public Ticket(String destination, int price)
+    {
+        this.destination=destination;
           this.price = price;
           timeStamp = new Date();
-      }
-      //print ticket method
-      public void printTicket()
-      {
-           System.out.println("##############################");
-           System.out.println("Destination:  " + destination + "  ");
-           System.out.println("#Price:        " + price + "pence        #");
-           System.out.println("#" + timeStamp + "#");
-           System.out.println("##############################");      
-      }
+    }
+      
+    /**
+    * This method will print the ticket.
+    * You can see the destination, price, date and time of purchase.
+    */
+    public void printTicket()
+    {
+        System.out.println("##############################");
+        System.out.println("Destination:  " + destination + "  ");
+        System.out.println("#Price:        " + price + "pence      #");
+        System.out.println("#" + timeStamp + "#");
+        System.out.println("##############################");      
+    }
       
 } 
