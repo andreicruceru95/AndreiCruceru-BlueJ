ad1d5c8b8d0a86e495606a18fa0c77a65e3fb36a
andreicruceru95 <56835442+andreicruceru95@users.noreply.github.com>
2020-10-12 21:49:48 +0100

    Update Module.java
    added a method that gives marks and calculates if the module was passed by the student based on the given marks

--- app03-studentGrades/Module.java
+++ app03-studentGrades/Module.java
@@ -1,12 +1,15 @@
 /**
+ * This class will create modules for a course.
+ * Each module will be given a mark and it will be considered completed if the mark is over 39%.
  * @author Andrei Cruceru
  * @version 12102020
  */
 public class Module
 {
-    private String title;
-    private int code;
-    private int marks;
+    public String title;
+    public int code;
+    public int mark = 0;
+    public boolean isCompleted;
     
     //Constructor method for Module
     public Module (String title, int code)
@@ -18,10 +21,42 @@ public class Module
     /**
      * This method will add points for this module
      */
-    public void giveMarks(int marks)
+    public void giveMark(int mark)
     {
-        this.marks = marks;
-        System.out.println(title + " " + code + " recieves " + marks + "%"); 
+        this.mark = mark;
+        if (mark > 39 && mark <= 100) 
+        {
+            isCompleted = true;
+        }
+        else if (mark > 100) 
+        {
+            System.out.println("Maximum mark you can give is 100");
+        }
+        else if (mark < 0) 
+        {
+            System.out.println("Please insert a positive number");
+        }
+        else
+        {
+            isCompleted = false;
+        }
+        System.out.println(title + " " + code + " recieves " + mark + "%"); 
+    }
+    
+    /**
+     * This method will tell us if the course is completed or not
+     */
+    public boolean isCompleted()
+    {
+        return isCompleted;
+    }
+    
+    /**
+     * This method will return the marks for a module
+     */
+    public int getMark()
+    {
+        return mark;
     }
     
     /**
@@ -29,6 +64,6 @@ public class Module
      */
     public void printModuleDetails()
     {
-        System.out.println("Module name: " + title + " Code: " + code + " Marks: " + marks +"%");
+        System.out.println("Module name: " + title + " Code: " + code + " Marks: " + mark +"%");
     }
 }
