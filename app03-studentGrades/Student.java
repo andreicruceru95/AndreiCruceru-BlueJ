c67c5ed99ce7e4865bfa802de5e590783d50ad26
andreicruceru95 <56835442+andreicruceru95@users.noreply.github.com>
2020-10-12 21:45:14 +0100

    Big Update
    added the missing variables(name, id)
    added a method to enrol a student to a course
    added a method that prints the student details
    added a method that prints the course details

--- app03-studentGrades/Student.java
+++ app03-studentGrades/Student.java
@@ -1,13 +1,50 @@
 /**
+ * This class will create a student with a student ID.
+ * It will enroll the student to a course and print the details of this course.
  * @author Andrei Cruceru
  * @version 12102020
  */
 
 public class Student
 {
-    //Student name
+    // Student name
     private String name;
-    //the course the student attends
+    // Student ID
+    private int studentId;
+    // The course the student attends
     private Course course;
     
+    /**
+     * Give a name to this student
+     */
+    public Student(String name, int studentId)
+    {
+        this.name = name;
+        this.studentId = studentId;
+    }
+    
+    /**
+     * This method will enrol a student to a course
+     */
+    public void enrolOnCourse(Course course)
+    {
+        this.course = course;
+    }
+    
+    /**
+     * This method will return the name and id of the student
+     */
+    public void getStudentDetails()
+    {
+        System.out.println("Name: " + name + " Student ID: " + studentId);
+    }
+    
+    /**
+     * This method will print the final grades and marks for the student
+     */
+    public void getCourseDetails()
+    {
+        course.printCourseDetails();
+    }
+
 }
