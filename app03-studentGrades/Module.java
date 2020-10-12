b367b4f5340acd9a8996fb9a41e91d922d76c3ec
andreicruceru95 <56835442+andreicruceru95@users.noreply.github.com>
2020-10-12 13:38:07 +0100

    Update Module.java
    added a giveMarks method that will give marks to a module
    added a print method that will print the details of the module

--- app03-studentGrades/Module.java
+++ app03-studentGrades/Module.java
@@ -7,4 +7,28 @@ public class Module
     private String title;
     private int code;
     private int marks;
+    
+    //Constructor method for Module
+    public Module (String title, int code)
+    {
+        this.title = title;
+        this.code = code;
+    }
+    
+    /**
+     * This method will add points for this module
+     */
+    public void giveMarks(int marks)
+    {
+        this.marks = marks;
+        System.out.println(title + " " + code + " recieves " + marks + "%"); 
+    }
+    
+    /**
+     * This method will print the full module details
+     */
+    public void printModuleDetails()
+    {
+        System.out.println("Module name: " + title + " Code: " + code + " Marks: " + marks +"%");
+    }
 }
