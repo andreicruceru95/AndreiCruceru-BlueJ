03364b4e776560ddf45e18985e3cee415ae22631
andreicruceru95 <56835442+andreicruceru95@users.noreply.github.com>
2020-10-12 17:40:59 +0100

    Student class update
    added the cunstructor method and working on creating the rest of methods

--- app03-studentGrades/Course.java
+++ app03-studentGrades/Course.java
@@ -1,9 +1,30 @@
+import java.util.*;
 /**
  * @author Andrei Cruceru
  * @version 12102020
  */
 public class Course
 {
-    private String name;
+    //course name
+    private String courseName;
+    //modules for course
     public Module module;
+    
+    private ArrayList<Module> modules;
+    
+    //constructor method
+    public Course(String courseName)
+    {
+        this.courseName = courseName;
+    }
+    
+    public void addFirstModule(Module newModule)
+    {
+        this.module = newModule;
+    }
+    
+    public void calculateGrade()
+    {
+        
+    }
 }
