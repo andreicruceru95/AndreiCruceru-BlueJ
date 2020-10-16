4b06ebe120dcb94b1a4ec55f3790bee981fe9b1b
andreicruceru95 <56835442+andreicruceru95@users.noreply.github.com>
2020-10-16 19:16:53 +0100

    Update Course.java
    Changed the four static variables for  an ArrayList

--- app03-studentGrades/Course.java
+++ app03-studentGrades/Course.java
@@ -1,6 +1,12 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
- * This class will create a course with four modules.
- * It will calculate the credits for the course and the overall grade of the four modules.
+ * This class represents a real life example of a course.
+ * A course has a three years lenght and eight modules each year separated in two semesters.
+ * It will calculate the credits for the course based on how many modules are completed and the overall grade of the modules.
+ * It also allows the user to search modules based on their code, or part of their name.
  * 
  * @author Andrei Cruceru
  * @version 12102020
@@ -8,70 +14,37 @@
 public class Course
 {
     // Course name
-    private String courseName;
+    private String name;
     // Course Code
-    private String courseCode;
-    // The four modules of the course
-    private Module moduleOne;
-    private Module moduleTwo;
-    private Module moduleThree;
-    private Module moduleFour;
+    private String code;
+    //ArrayList of modules
+    public ArrayList<Module> modules;
+    private Module module;
     // Course credits
     private int credits = 0;
     // Marks for the modules
     private int mark = 0;
+    private int finalMark = 0;
     // The final grade of the course
     private String finalGrades;
          
     /**
      * Add a name and code to this course.
      */
-    public Course(String courseName, String courseCode)
+    public Course(String name, String code)
     {
-        this.courseName = courseName;
-        this.courseCode = courseCode;
+        this.name = name;
+        this.code = code;
+        modules = new ArrayList<Module>();
+        finalGrades = null;
     }
     
     /**
-     * Add module one to this course.
+     * Add a module to the list.
      */
-    public void addModuleOne(Module newModule)
+    public void addModule(Module newModule)
     {
-        moduleOne = newModule;
-    }
-    
-    /**
-     * Add module Two to this course.
-     */
-    public void addModuleTwo(Module newModule)
-    {
-        moduleTwo = newModule;
-    }
-    
-    /**
-     * Add module three to this course.
-     */
-    public void addModuleThree(Module newModule)
-    {
-        moduleThree = newModule;
-    }
-    
-    /**
-     * Add module four to this course.
-     */
-    public void addModuleFour(Module newModule)
-    {
-        moduleFour = newModule;
-    }
-    
-    /**
-     * This method will calculate the final grades of the course.
-     * The student receives 15 credits for each module with a mark over 39%.
-     */
-    public void calculateCredits()
-    {
-        credits = moduleOne.getCredits() + moduleTwo.getCredits() + moduleThree.getCredits() + moduleFour.getCredits();
-        System.out.println("This course has acumulated " + credits + " credits");
+        modules.add(newModule);
     }
     
     /**
@@ -80,43 +53,49 @@ public class Course
      */
     public void calculateFinalGrades()
     {
-        if (moduleOne.isMarked == true && moduleTwo.isMarked == true
-            && moduleThree.isMarked == true && moduleFour.isMarked == true)
+        modules.forEach(module ->
         {
-            // We calculate the average mark of the four modules
-            mark = moduleOne.getMark() + moduleTwo.getMark() + moduleThree.getMark() + moduleFour.getMark();
-            mark = mark / 4;
-            // We convert the mark into grades
-            if (mark <= 39)
-            {
-                finalGrades = "F";
-            }
-            else if (mark > 39 && mark <= 49)
-            {
-                finalGrades = "D";
-            }
-            else if (mark > 49 && mark <= 59)
-            {
-                finalGrades = "C";
-            }
-            else if (mark > 59 && mark <= 69)
-            {
-                finalGrades = "B";
-            }
-            else
-            {
-                finalGrades = "A";
-            }
-        
-            System.out.println("Your final marks are: " + mark + "%");
-            System.out.println("Your final grades are: " + finalGrades);
-            System.out.println("your final credits are: " + credits);
-        }
-        else 
+            mark = mark + module.getMark();
+            credits = credits + module.getCredits();
+        });
+        System.out.println("Your total marks for your " + modules.size() + " modules is: " + mark); 
+        finalMark = mark / modules.size(); 
+        // Converting marks to grades
+        if (finalMark < 40 && finalMark > 0)
         {
-            System.out.println("You can calculate the final mark and grades");
-            System.out.println("once all the modules have been marked");
+            finalGrades = "F";
         }
+        else if (finalMark > 39 && finalMark < 50)
+        {
+            finalGrades = "D";
+        }
+        else if (finalMark > 49 && finalMark < 60)
+        {
+            finalGrades = "C";
+        }
+        else if (finalMark > 59 && finalMark < 70)
+        {
+            finalGrades = "B";
+        }
+        else if (finalMark > 69 && finalMark < 100)
+        {
+            finalGrades = "A";
+        }
+        else
+        {
+            System.out.println("Module marks should be a positive between 0 and 100!");
+        }
+        System.out.println("Your final mark is: " + finalMark + "%");
+        System.out.println("Your final grade is: " + finalGrades);
+        System.out.println("Your final credits are: " + credits);
+    }
+    
+    /**
+     * This method will return the course details only.
+     */
+    public String getCourse()
+    {
+        return "Course name: " + name + ", Course code: " + code;
     }
         
     /**
@@ -125,11 +104,58 @@ public class Course
      */
     public void printCourseDetails()
     {
-        System.out.println("Course: " + courseName + " Course code " + courseCode + " Credits: " + credits);
-        System.out.println("Modules and mark:");
-        moduleOne.printModuleDetails();
-        moduleTwo.printModuleDetails();
-        moduleThree.printModuleDetails();
-        moduleFour.printModuleDetails();
+        System.out.println("Course name: " + name + ", Course code: " + code);
+        System.out.println("Modules:");
+        modules.forEach(module ->
+        {
+            System.out.println(module.getTitle() + "," + module.getCode() + ",Mark: " + module.getMark() + ", Credits: " + module.getCredits());
+        });
+    }
+    
+    /**
+     * Return the final mark value.
+     */
+    public void getFinalMark()
+    {
+        System.out.println("Final mark: " + finalMark + "%");
+    }
+    
+    /**
+     * Search for modules using it's code.
+     * A module code is made by year number and semester number (ex:Y1S1).
+     * As another example Y1 will show you all the modules from year 1, S1 will show you all the modules from semesters 1.
+     */
+    public void searchCode(String code)
+    {
+        List <Module> listClone = new ArrayList<Module>(); 
+        for(Module module : modules) 
+        { 
+            if(module.getCode().contains(code))
+ 
+            { 
+                listClone.add(module);
+            }
+        }
+        listClone.forEach(entry->
+        {
+            System.out.println(entry.getTitle() + "," + entry.getCode() + ",Mark: " + entry.getMark() + ", Credits: " + entry.getCredits());
+        });
+    }
+    
+    public void searchName(String word)
+    {
+        List <Module> listClone = new ArrayList<Module>(); 
+        for(Module module : modules)
+        { 
+            if(module.getTitle().contains(word))
+ 
+            { 
+                listClone.add(module);
+            }
+        }
+        listClone.forEach(entry->
+        {
+            System.out.println(entry.getTitle() + "," + entry.getCode() + ",Mark: " + entry.getMark() + "%" + ", Credits: " + entry.getCredits());
+        });
     }
 }
