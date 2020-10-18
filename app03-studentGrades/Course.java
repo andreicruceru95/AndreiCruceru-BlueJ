d49069ebae5ae142911de06f7f5b195ff491985f
andreicruceru95 <56835442+andreicruceru95@users.noreply.github.com>
2020-10-18 17:56:58 +0100

    Update Course.java
    Added the rest of the constants
    Added a method that calculates the grades for a given period of time ex for each semester or for each year.

--- app03-studentGrades/Course.java
+++ app03-studentGrades/Course.java
@@ -14,6 +14,10 @@ import java.util.List;
 public class Course
 {
     public static final int LOWEST_PASS = 40;
+    public static final int D_PASS = 50;
+    public static final int C_PASS = 60;
+    public static final int B_PASS = 70;
+    public static final int MAXIMUM_MARK = 100;
     // Course name
     private String name;
     // Course Code
@@ -68,7 +72,7 @@ public class Course
     }
     
     /**
-     * 
+     * This method will print out the final grades, marks and credits
      */
     public void printFinalGrades()
     {
@@ -79,29 +83,29 @@ public class Course
     }
        
     /**
-     * 
+     * Convert the marks into grades.
      */
     public Grades convertToGrade(int mark)
     {
         Grades grade = null;
-        
+                
         if (mark < LOWEST_PASS && mark > 0)
         {
             grade = Grades.F;
         }
-        else if (mark >= LOWEST_PASS  && mark < 50)
+        else if (mark >= LOWEST_PASS  && mark < D_PASS)
         {
             grade = Grades.D;
         }
-        else if (mark > 49 && mark < 60)
+        else if (mark > D_PASS && mark < C_PASS)
         {
             grade = Grades.C;
         }
-        else if (mark > 59 && mark < 70)
+        else if (mark > C_PASS && mark < B_PASS)
         {
             grade = Grades.B;
         }
-        else if (mark > 69 && mark < 100)
+        else if (mark > B_PASS && mark < MAXIMUM_MARK)
         {
             grade = Grades.A;
         }
@@ -111,6 +115,7 @@ public class Course
         }
          
         return grade;
+        
     }
     
     /**
@@ -133,6 +138,7 @@ public class Course
         {
             System.out.println(module.getTitle() + "," + module.getCode() + ",Mark: " + module.getMark() + ", Credits: " + module.getCredits());
         });
+        
     }
     
     /**
@@ -140,11 +146,11 @@ public class Course
      */
     public void getmark()
     {
-        System.out.println("Final mark: " + mark + "%");
+        System.out.println("Final mark: " + finalMark + "%");
     }
     
     /**
-     * Search for modules using it's code.
+     * Search for modules using their code.
      * A module code is made by year number and semester number (ex:Y1S1).
      * As another example Y1 will show you all the modules from year 1, S1 will show you all the modules from semesters 1.
      */
@@ -159,13 +165,18 @@ public class Course
                 listClone.add(module);
             }
         }
+        
         listClone.forEach(entry->
         {
             System.out.println(entry.getTitle() + "," + entry.getCode() + ",Mark: " + entry.getMark() + ", Credits: " + entry.getCredits());
         });
+        
     }
     
-    public void searchName(String word)
+    /**
+     * This method will search the modules for the input word.
+     */
+    public void searchModule(String word)
     {
         List <Module> listClone = new ArrayList<Module>(); 
         for(Module module : modules)
@@ -176,9 +187,49 @@ public class Course
                 listClone.add(module);
             }
         }
+        
         listClone.forEach(entry->
         {
             System.out.println(entry.getTitle() + "," + entry.getCode() + ",Mark: " + entry.getMark() + "%" + ", Credits: " + entry.getCredits());
         });
+        
+    }
+    
+    /**
+     * Calculate the grades for a semester or year.
+     */
+    public void calculateForPeriod(String code)
+    {
+        this.mark = 0;
+        credits = 0;
+               
+        List <Module> listClone = new ArrayList<Module>(); 
+        for(Module module : modules) 
+        { 
+            if(module.getCode().contains(code))
+ 
+            { 
+                listClone.add(module);
+            }
+        }
+        
+        listClone.forEach(entry->
+        {
+            this.mark = mark + entry.getMark();
+            credits = credits + entry.getCredits(); 
+        });    
+                
+        System.out.println("For the given period, you have obtained " + mark/listClone.size() + "% mark and " + credits + " Credits.");
+        finalMark = mark/listClone.size();
+        finalGrade = convertToGrade(finalMark);
+        
+        System.out.println("Your grade for the time period is " + finalGrade + " ");
+        System.out.println("Here is a breakdown of your modules for the time given:");
+            
+        listClone.forEach(entry->
+        {
+            System.out.println(entry.getTitle() + "," + entry.getCode() + ",Mark: " + entry.getMark() + "%" + ", Credits: " + entry.getCredits());
+        });
+        
     }
 }
