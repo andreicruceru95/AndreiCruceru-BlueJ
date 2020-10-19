8fad256eb595443e4c6ddff4e52ba67979bcc586
andreicruceru95 <56835442+andreicruceru95@users.noreply.github.com>
2020-10-19 22:13:09 +0100

    Documentation

---
+++ app03-studentGrades/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Course"},{"p":"<Unnamed>","l":"Grades"},{"p":"<Unnamed>","l":"Module"},{"p":"<Unnamed>","l":"ModuleDemo"},{"p":"<Unnamed>","l":"Student"}]
