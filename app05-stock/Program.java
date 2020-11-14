d39bf1e2ad82de385864302153d36ab46ce91619
andreicruceru95 <56835442+andreicruceru95@users.noreply.github.com>
2020-11-14 17:40:48 +0000

    Update Program.java

--- app05-stock/Program.java
+++ app05-stock/Program.java
@@ -1,21 +1,20 @@
-import java.awt.Color; 
-import enigma.console.*; 
-import enigma.core.Enigma;
-
 /**
  * Write a description of class Program here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Andrei Cruceru
+ * @version 14112020
  */
 public class Program
 {
     private static final char CLEAR = '\u000c';
+    
     private static StockApp app;
 
     /**
      * This class creates and runs an instance of
      * the StockApp class
+     * 
+     * Clear the terminal before each use.
      */
     public static void main()
     {
