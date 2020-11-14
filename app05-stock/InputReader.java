5121715089cde16f38a7bcb24ff9f557425428c1
andreicruceru95 <56835442+andreicruceru95@users.noreply.github.com>
2020-11-14 14:46:18 +0000

    Update InputReader.java

--- app05-stock/InputReader.java
+++ app05-stock/InputReader.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
  * InputReader reads typed text input from the standard text terminal. 
  * The text typed by a user is returned.
  * 
- * @author     Michael Kölling and David J. Barnes
- * @version    0.1 (2016.02.29)
+ * @author     Andrei Cruceru
+ * @version    13112020
  */
 public class InputReader
 {
@@ -26,11 +26,40 @@ public class InputReader
      *
      * @return  A String typed by the user.
      */
-    public String getInput()
+    public String getString()
     {
         System.out.print("> ");         // print prompt
         String inputLine = reader.nextLine();
 
         return inputLine;
     }
+    
+    /**
+     * Read a line from standard input (the text terminal),
+     * and return it as a Integer.
+     *
+     * @return  A Integer typed by the user. 
+     */
+    public Integer getInteger()
+    {
+        System.out.print("> ");
+        int inputLine = reader.nextInt();
+        
+        return inputLine;
+    }
+    
+    // public int scan()
+    // {
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Please enter product id!");
+        // while (!sc.hasNextInt()) {
+            // System.out.println("Cannot leave blank");
+            // sc.next();
+        // }
+        // int id = sc.nextInt();
+        // System.out.println("id accepted");
+        // return id;
+    // }
 }
+    
+
