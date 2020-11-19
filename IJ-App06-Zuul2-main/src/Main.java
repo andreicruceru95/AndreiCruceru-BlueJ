3f7bf6512a21e2a859f996aabe277b07381bf7f9
andreicruceru95 <56835442+andreicruceru95@users.noreply.github.com>
2020-11-19 18:57:34 +0000

    test2

---
+++ IJ-App06-Zuul2-main/src/Main.java
@@ -0,0 +1,16 @@
+/**
+ *
+ */
+public class Main
+{
+    private static Game zuul;
+
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        zuul = new Game();
+    }
+}
