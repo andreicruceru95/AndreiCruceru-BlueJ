0edd80fed76c50edd686a7eb307c60e1c1005c82
andreicruceru95 <56835442+andreicruceru95@users.noreply.github.com>
2020-11-28 13:43:14 +0000

    Update World.java

--- App06-Zuul2/World.java
+++ App06-Zuul2/World.java
@@ -1,14 +1,12 @@
 import java.util.*;
 
 /**
- * Write a description of class World here.
+ * Write a description of class Map here.
  *
  * @author (your name)
  * @version (a version number or a date)
  */
 public class World
 {
-    private HashMap <String, Map> world = new HashMap<String, Map>();
-    
-    
+    private Map map;
 }
